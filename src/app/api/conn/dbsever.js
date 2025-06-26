51edb0be96def83ba0b71c230d74f0f1d880ca3c
userti <ti@touticosmetics.com.br>
2025-06-26 15:45:01 -0300

    ajuste dbsever

--- src/app/api/conn/dbsever.js
+++ src/app/api/conn/dbsever.js
@@ -1,12 +1,11 @@
-require('dotenv').config();
 const sql = require('mssql')
 
 const config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  server: process.env.DB_HOST, // ou IP]
-  port:parseInt(process.env.DB_PORT, 10),
-  database: process.env.DB_NAME,
+  user: 'rubens.pereira',
+  password: 'Touti#2025',
+  server: '191.234.202.14', // ou IP]
+  port:14333,
+  database: 'promocao_amostra',
   options: {
     encrypt: false, // true se for Azure
     trustServerCertificate: true,
